a275531e9bcc9a3d601c81afc9a2388f6b6da091
hfellerhoff <henryfellerhoff@gmail.com>
2024-09-16 12:20:15 -0400

    remove log

--- src/lib/fft.ts
+++ src/lib/fft.ts
@@ -16,8 +16,6 @@ export const binSizeHzAtom = atom(
   (get) => get(sampleRateAtom) / get(binSizeAtom)
 );
 
-console.log(devicePixelRatio);
-
 export const heightFactor = atom((get) => {
   return get(fftSizeAtom) / (546.1333333333 * devicePixelRatio);
 });
